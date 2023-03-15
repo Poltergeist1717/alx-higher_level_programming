d05240887eb808c07a3da38575f59ba0a2ef1370
Olamide Yakub Lateef <98460197+Poltergeist1717@users.noreply.github.com>
2023-03-15 06:06:38 +0100

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    """Print names defined by a module"""
-    import hidden_4
-for str in dir(hidden_4):
-    if "__" not in str:
-        print(str)
